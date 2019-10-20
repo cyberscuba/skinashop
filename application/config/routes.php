2e9e9817b8eba73ed747fc73f0cd6125ceb400fe
Carlos Andrés Martínez Salcedo <CyberScuba@MacBook-Pro.local>
2019-10-20 17:02:20 -0500

    Cambiando la ruta

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = 'products_category_controller';
+$route['default_controller'] = 'Total_products_controller';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
